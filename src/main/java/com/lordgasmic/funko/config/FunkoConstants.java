9df9f3386bfde84487ecfeb2c581679aa12e84c4
Nick Wellman <lordgasmic@lordgasmic.com>
2024-07-31 13:18:40 -0400

    fix name of prop

--- src/main/java/com/lordgasmic/funko/config/FunkoConstants.java
+++ src/main/java/com/lordgasmic/funko/config/FunkoConstants.java
@@ -6,6 +6,6 @@ public final class FunkoConstants {
     public static final String PROP_ID = "id";
     public static final String PROP_TITLE = "title";
     public static final String PROP_FANDOM = "fandom";
-    public static final String PROP_SERIES_ID = "series_id";
+    public static final String PROP_SERIES_ID = "seriesId";
     public static final String PROP_NAME = "name";
 }
