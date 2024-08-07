12a5cc4a764c7cb410dcc409b9ebec991f88ca7f
Nick Wellman <lordgasmic@lordgasmic.com>
2024-08-07 16:26:44 -0400

    child docs

--- src/main/java/com/lordgasmic/funko/service/FunkoIndexService.java
+++ src/main/java/com/lordgasmic/funko/service/FunkoIndexService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -38,19 +39,19 @@ public class FunkoIndexService {
             document.addField("fandom", funko.getFandom());
             document.addField("seriesId", funko.getSeriesId());
             document.addField("name", funko.getName());
-            List<Integer> extras = new ArrayList<>();
-//            List<SolrInputDocument> extras = new ArrayList<>();
+//            List<Integer> extras = new ArrayList<>();
+            List<SolrInputDocument> extras = new ArrayList<>();
             for (FunkoExtrasResponse funkoExtras : funko.getExtras()) {
-//                SolrInputDocument doc = new SolrInputDocument();
-//                doc.addField("id", UUID.randomUUID().toString());
-//                doc.addField("extraId", funkoExtras.getId());
-//                doc.addField("funkoId", funkoExtras.getFunkoId());
-//                doc.addField("text", funkoExtras.getText());
+                SolrInputDocument doc = new SolrInputDocument();
+                doc.addField("id", UUID.randomUUID().toString());
+                doc.addField("extraId", funkoExtras.getId());
+                doc.addField("funkoId", funkoExtras.getFunkoId());
+                doc.addField("text", funkoExtras.getText());
 //                document.addChildDocument(doc);
-//                extras.add(doc);
+                extras.add(doc);
 
 
-                extras.add(funkoExtras.getFunkoId());
+//                extras.add(funkoExtras.getFunkoId());
             }
             document.addField("extras", extras);
             client.add(document);
