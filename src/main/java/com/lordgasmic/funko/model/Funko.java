5260931250fb8c2e445d35af1f5b37cf4826a541
Nick Wellman <wellman.nicholas@gmail.com>
2026-01-26 23:44:11 -0500

    add some constructors to models

--- src/main/java/com/lordgasmic/funko/model/Funko.java
+++ src/main/java/com/lordgasmic/funko/model/Funko.java
@@ -1,13 +1,17 @@
 package com.lordgasmic.funko.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Funko {
     private long id;
     private String title;
--- src/main/java/com/lordgasmic/funko/model/FunkoExtra.java
+++ src/main/java/com/lordgasmic/funko/model/FunkoExtra.java
@@ -1,10 +1,14 @@
 package com.lordgasmic.funko.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FunkoExtra {
     private long id;
     private long funkoId;
